080bbc32ba9ee17f24ec3f85576a94f9a34be53d
Md Taqib <75027833+aman868@users.noreply.github.com>
2023-10-07 18:48:06 +0530

    Add files via upload

---
+++ ai.py
@@ -0,0 +1,86 @@
+from xml.dom.minidom import CharacterData
+import pyttsx3
+import datetime
+import speech_recognition
+import wikipedia
+import wolframalpha
+import os
+
+engine= pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+print(voices[2].id)
+engine.setProperty('voice',voices[2].id)
+engine.setProperty('rate', 150)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def time():
+    Time = datetime.datetime.now().strftime("%H:%M")
+    speak(Time)
+
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    day = int(datetime.datetime.now().day)
+    speak(day)
+    speak(month)
+    speak(year)
+
+
+
+speak("I am ready to help. Waiting for your command")
+
+def takecommand():
+    r = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        print("************ ...........Listening.......... ************")
+        r.pause_threshold = 1
+        r.energy_threshold = 200
+        audio = r.listen(source)
+
+    try:
+        print("*********........... RECONGNIZNING ...........*********")
+        query = r.recognize_google(audio, language='en-in')
+        print(query)
+
+    except Exception as e:
+        print(e)
+        speak("Say that again please....")
+        return "none"
+    return query
+
+if __name__ == "__main__":
+    while True:
+        question = takecommand().lower()
+        while True:
+                query = takecommand().lower()
+
+                if 'wikipedia' in query:
+                    speak('Searching Wikipedia....')
+                    query =query.replace("Wikipedia","")
+                    results = wikipedia.summary(query, sentences=2)
+                    speak("According to Wikipedia......")
+                    speak(results)
+                elif 'google' in query:
+                    speak('searching google....')
+                    query =query.replace("google","")
+                    results = googlesearch.summary(query, sentence=2)
+                    speak("According to google......")
+                    print(results)
+                    speak(results)
+                elif 'wolframalpha' in query:
+                    speak('searching wolframalpha.....')
+                    query =query.replace("wolframalpha","")
+                    results =wolframalpha.summary(query, sentence=2)
+                    speak("Accirding to wolframalpha......")
+                    speak(results)
+                elif 'Time' in query:
+                    speak("Time ")
+                    time()
+                elif "exit" in query:
+                    pyttsx3.speak(" ")
+                    break
+                pyttsx3.speak("Thank you for using me.")
+    
